386997b4d4b0ddce65980cab4b60d440e7b5197a
lalaantika <sharmal0707@gmail.com>
2020-04-09 16:17:35 -0400

    javs script for the functioning of buttons

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 //"use strict";
 
 function insert(num) {
-  document.calcform.textview.value = document.calcform.textview.value+num
+    document.calcform.textview.value = document.calcform.textview.value + num
+}
+
+function equal() {
+    var exp = document.calcform.textview.value;
+    if (exp) {
+        document.calcform.textview.value = eval(exp)
+    }
+}
+
+function clean() {
+    document.calcform.textview.value = "";
 }
